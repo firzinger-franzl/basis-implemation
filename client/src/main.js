e613ea5d0114429aaf2c55a875bee5e82a04f702
Hausmeister <71006690+firzinger-franzl@users.noreply.github.com>
2021-05-05 09:41:48 +0200

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import VueCookies from 'vue-cookies';
 
 
 Vue.config.productionTip = false
-Vue.use(VueCookies);
+Vue.use(VueCookies); // wird benötigt weil sonst nichts gehen und ich habe es vergessen, weil ich nicht gerade intelligent bin
 
 
 new Vue({
