72e90b0c4c5d75c637a8dbf070ded654e5b34235
Hausmeister <71006690+firzinger-franzl@users.noreply.github.com>
2021-05-05 11:11:00 +0200

    Wtf, ich hatte einfach das eine im users.js

--- server/model/users.js
+++ server/model/users.js
@@ -18,12 +18,4 @@ const users = [
     password: '1234',
   },
 ];
-if (email && password) {
- const user = users.find(el => el.email === email &&
- el.password === password);
- if (user) {
- req.session.userId = user.id;
- res.status(200).json({ id: user.id, name: user.name });
- } else res.status(401).send('Wrong email or password');
-} else res.status(400).send('Login failed')
 module.exports = users;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,17 +4,16 @@ const router = express.Router();
 
 router.post('/login', (req, res) => {
   let password = req.body.password;
-  let email = req.body.email;
+  let email = req.body.email; 
   if (email && password) {
-    const user = users.find((el => el.email === email && el.password === password));
+    const user = users.find((el => el.email === email && el.password === password)); 
     if (user) {
       req.session.userID = user.id;
       res.status(200).json({id: user.id, name: user.name});} 
       else res.status(401).send('Falsche Email oder falsches Passwort');
     } else res.status(400).send('Login fehlgeschlagen')
   }
-
-});
+);
 
 router.get('/logout', /*redirectLogin*/ (req, res) => {
   req.session.destroy();
